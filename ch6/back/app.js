2c3fc43b4fb8665ccf9bbdc0487d30a13dc8b0cd
zerocho <zerohch0@gmail.com>
2019-09-08 22:34:26 +0900

    Update: dotenv

--- ch6/back/app.js
+++ ch6/back/app.js
@@ -4,7 +4,11 @@ const passport = require('passport');
 const session = require('express-session');
 const cookie = require('cookie-parser');
 const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
 
+const prod = process.env.NODE_ENV === 'production';
 const db = require('./models');
 const passportConfig = require('./passport');
 const userRouter = require('./routes/user');
@@ -13,25 +17,38 @@ const postsRouter = require('./routes/posts');
 const hashtagRouter = require('./routes/hashtag');
 const app = express();
 
+dotenv.config();
 db.sequelize.sync();
 passportConfig();
 
-app.use(morgan('dev'));
-app.use(cors({
-  origin: 'http://localhost:3081',
-  credentials: true,
-}));
+if (prod) {
+  app.use(helmet());
+  app.use(hpp());
+  app.use(morgan('combined'));
+  app.use(cors({
+    origin: 'http://vue.nodebird.com',
+    credentials: true,
+  }));
+} else {
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: 'http://localhost:3081',
+    credentials: true,
+  }));
+}
+
 app.use('/', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookie('cookiesecret'));
+app.use(cookie(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
   saveUninitialized: false,
-  secret: 'cookiesecret',
+  secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
+    domain: prod && '.nodebird.com',
   },
 }));
 app.use(passport.initialize());
@@ -46,6 +63,6 @@ app.use('/post', postRouter);
 app.use('/posts', postsRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3085, () => {
-  console.log(`백엔드 서버 ${3085}번 포트에서 작동중.`);
+app.listen(prod ? process.env.PORT : 3085, () => {
+  console.log(`백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동중.`);
 });
