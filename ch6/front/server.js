1d64c4442f5c2114b8a48ebc61096ac7ffd69745
zerocho <zerohch0@gmail.com>
2019-09-14 15:35:31 +0900

    Update: to 3081 port

--- ch6/front/server.js
+++ ch6/front/server.js
@@ -2,7 +2,7 @@ const { Nuxt, Builder } = require('nuxt');
 
 const app = require('express')();
 const isProd = (process.env.NODE_ENV === 'production');
-const port = process.env.PORT || 3085;
+const port = process.env.PORT || 3081;
 
 // We instantiate Nuxt.js with the options
 const config = require('./nuxt.config.js');
